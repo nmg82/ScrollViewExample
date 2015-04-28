3fb14da2bfea97d814decbada5bd0f3859f243ed
Nathan Gladson <nathan@egovtn.org>
2015-04-28 12:25:56 -0500

    Initial Commit

---
+++ DetailsScreenTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DetailsScreenTest
+//
+//  Created by Nathan Gladson on 4/28/15.
+//  Copyright (c) 2015 NICUSA Tennesse Division. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
